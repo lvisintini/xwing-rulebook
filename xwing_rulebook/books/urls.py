e96d11acb45a3cb51b121e6f180a0aa7fc4c0722
Luis Visintini <lvisintini@gmail.com>
2017-03-08 14:18:53 +0000

    Better book view and urls

--- xwing_rulebook/books/urls.py
+++ xwing_rulebook/books/urls.py
@@ -6,5 +6,6 @@ app_name = 'books'
 urlpatterns = [
     url(r'^(?P<book_slug>[\w-]+)/$', views.book, name='book'),
     url(r'^(?P<book_slug>[\w-]+)/(?P<section_slug>[\w-]+)/$', views.book, name='section'),
+    url(r'^(?P<book_slug>[\w-]+)/(?P<rule_slug>[\w-]+)/$', views.book, name='rule-in-book'),
     url(r'^(?P<book_slug>[\w-]+)/(?P<section_slug>[\w-]+)/(?P<rule_slug>[\w-]+)/$', views.book, name='rule'),
 ]
--- xwing_rulebook/books/views.py
+++ xwing_rulebook/books/views.py
@@ -1,43 +1,46 @@
-from django.http import Http404
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 
 from books.models import Book, Section, SectionRule
-from rules.models import Rule
 
 
 def book(request, book_slug, section_slug=None, rule_slug=None):
-    context = {
-        'book': get_object_or_404(Book, slug=book_slug)
-    }
+    book = get_object_or_404(Book, slug=book_slug)
 
-    if not section_slug:
-        return render(request, 'book.html', context)
+    if not section_slug and not rule_slug:
+        return render(request, 'book.html', {'book': book})
 
-    context['section'] = Section.objects.filter(slug=section_slug, book=context['book']).first()
-
-    if not rule_slug:
-        if context['section'] is not None:
-            return render(request, 'section.html', context)
-
-        # If the section was not found, try to use section_slug as a rule_slug and find the section.
-        # The redirect to the book:rule url.
-        rule_slug = section_slug
-        try:
-            section_slug = SectionRule.objects.select_related('section').get(
-                section__book=book,
-                rule__slug=rule_slug,
-            ).section.slug
-        except SectionRule.DoesNotExist:
-            raise Http404
+    elif section_slug and not rule_slug:
+        section = Section.objects.filter(slug=section_slug, book=book).first()
+        if section:
+            return render(request, 'section.html', {'book': book, 'section': section})
         else:
-            redirect(
-                'boos:rule', book_slug=book.slug, section_slug=section_slug, rule_slug=rule_slug
+            section_rule = get_object_or_404(
+                SectionRule, section__book=book, rule__slug=section_slug
             )
 
-    context['rule'] = get_object_or_404(Rule, slug=rule_slug)
-
-    return render(request, 'book_rule.html', context)
-
+            section = section_rule.section
+            rule = section_rule.rule
+            return render(
+                request, 'book_rule.html', {'book': book, 'section': section, 'rule': rule}
+            )
+    elif not section_slug and rule_slug:
+            section_rule = get_object_or_404(
+                SectionRule, section__book=book, rule__slug=rule_slug
+            )
+            section = section_rule.section
+            rule = section_rule.rule
+            return render(
+                request, 'book_rule.html', {'book': book, 'section': section, 'rule': rule}
+            )
+    elif section_slug and rule_slug:
+            section_rule = get_object_or_404(
+                SectionRule, section__book=book, rule__slug=rule_slug, section__slug=section_slug
+            )
+            section = section_rule.section
+            rule = section_rule.rule
+            return render(
+                request, 'book_rule.html', {'book': book, 'section': section, 'rule': rule}
+            )
 
 def single_page_book(request):
     book = Book.objects.first()
