400a8b1fecb9a29402b12a6c000dcc11242354b6
Luis Visintini <lvisintini@gmail.com>
2017-07-04 11:56:55 +0100

    Final metadata

--- xwing_rulebook/pages/metadata.py
+++ xwing_rulebook/pages/metadata.py
@@ -1,62 +1,28 @@
 from django.conf import settings
 from django.urls import reverse
 from django.contrib.staticfiles.templatetags.staticfiles import static
+
+from rules.constants import CLAUSE_GROUPS, RULE_TYPES, CARD_TYPES
 from utils.lib import site_url_from_request
 
 
 class Metadata:
-    metas = [
-        "title",
-        "description",
-        "keywords",
-        "charset",
-        "author",
-        "robots",
-        "viewport",
-
-        "og_title",
-        "og_description",
-        "og_image",
-        "og_url",
-
-        "twitter_card",
-        "twitter_site",
-        "twitter_creator",
-        "twitter_title",
-        "twitter_description",
-        "twitter_image",
-
-        "msapplication_TileColor",
-        "msapplication_TileImage",
-        "msapplication_config",
-        "theme_color",
-        "apple_touch_icon",
-        "icon",
-        "manifest",
-        "mask_icon",
-        "shortcut_icon",
-
-        "canonical",
-    ]
+    separator = ' | '
 
     def __init__(self, context):
         self.context = context
 
-    def __str__(self):
-        metas = []
-        for m in self.metas:
-            data = getattr(self, m)()
-            if data:
-                metas.append(data)
-        return '\n'.join(metas)
+    def absolute_url(self, relative_url):
+        return site_url_from_request(self.context['request']) + relative_url
 
 
 class DefaultMetaData(Metadata):
-    default_title = "X-Wing Rulebook"
-    default_description = 'XWing Rulebook is a consolidated set of all the rules for the XWing Miniatures Game.'
+    default_title = ["X-Wing Rulebook", ]
+    default_description = 'A consolidated and complete list of all the rules for the XWing Miniatures Game.'
     default_keywords = [
-        'Wing Miniatures Game',
-        'XWing Miniatures',
+        'X-Wing Miniatures Game',
+        'X-Wing Miniatures',
+        'X-Wing',
         'XWing',
         'rules',
         'rulebook',
@@ -64,147 +30,173 @@ class DefaultMetaData(Metadata):
         'consolidated rules',
     ]
     default_og_type = "website"
-    default_twitter_handle = None
+    default_site_twitter_handle = None
 
     def title(self):
-        return "<title>{}</title>".format(self.default_title)
+        return self.default_title[0]
 
     def description(self):
-        return '<meta name="description" content="{}" />'.format(self.default_description)
+        return self.default_description
 
     def keywords(self):
-        return '<meta name="keywords" content="{}" />'.format(','.join(self.default_keywords))
+        return ','.join(self.default_keywords)
 
     def charset(self):
-        return '<meta charset="utf-8" />'
+        return "utf-8"
 
     def author(self):
-        return '<meta name="author" content="Luis Visintini" />'
+        return "Luis Visintini"
 
     def robots(self):
-        return '<meta name="robots" content="index, follow" />'
+        return "index, follow"
 
     def viewport(self):
-        return '<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />'
+        return "width=device-width, initial-scale=1, user-scalable=no"
 
     def og_title(self):
-        return '<meta property="og:title" content="{}" />'.format(self.default_title)
+        return self.default_title[0]
 
     def og_description(self):
-        return '<meta property="og:description" content="{}" />'.format(self.default_description)
+        return self.default_description
 
     def og_type(self):
-        return '<meta property="og:type" content="{}" />'.format(self.default_og_type)
+        return self.default_og_type
 
     def og_image(self):
-        return '\n'.join([
-            '<meta property="og:image" content="{image_url}" />',
-            '<meta property="og:image:secure_url" content="{image_url}" />',
-            '<meta property="og:image:type" content="{type}" />',
-            '<meta property="og:image:width" content="{width}" />',
-            '<meta property="og:image:height" content="{height}" />',
-        ]).format(
-            image_url=site_url_from_request(self.context['request']) + static('images/site/logo-200x200.png'),
-            type='image/png',
-            width='200',
-            height='200',
-        )
+        return {
+            "url": self.absolute_url(static('images/site/logo-200x200.png')),
+            "secure_url": self.absolute_url(static('images/site/logo-200x200.png')),
+            "type": 'image/png',
+            "width": '200',
+            "height": '200',
+        }
 
     def og_url(self):
-        return '<meta property="og:url" content="{}" />'.format(
-            site_url_from_request(self.context['request']) + self.context['request'].path
-        )
+        return self.absolute_url(self.context['request'].path)
 
     def twitter_card(self):
-        return '<meta name="twitter:card" content="summary" />'
+        return "summary"
 
     def twitter_site(self):
-        if self.default_twitter_handle:
-            return '<meta name="twitter:site" content="{}" />'.format(self.default_twitter_handle)
-        return
+        return self.default_site_twitter_handle
 
     def twitter_creator(self):
-        return '<meta name="twitter:creator" content="@lvisintini" />'
+        return "@lvisintini"
 
     def twitter_title(self):
-        return '<meta name="twitter:title" content="{}" />'.format(self.default_title)
+        return self.default_title[0]
 
     def twitter_description(self):
-        return '<meta name="twitter:description" content="{}" />'.format(self.default_description)
+        return self.default_description
 
     def twitter_image(self):
-        return '<meta name="twitter:image" content="{}" />'.format(
-            site_url_from_request(self.context['request']) + static('images/site/logo-200x200.png')
-        )
+        return self.absolute_url(static('images/site/logo-200x200.png'))
 
     def msapplication_TileColor(self):
-        return '<meta name="msapplication-TileColor" content="{}" />'.format(settings.BRAND_COLOR)
+        return settings.BRAND_COLOR
 
     def msapplication_TileImage(self):
-        return '<meta name="msapplication-TileImage" content="{}" />'.format(
-            site_url_from_request(self.context['request']) + static('favicons/mstile-144x144.png')
-        )
+        return self.absolute_url(static('favicons/mstile-144x144.png'))
 
     def msapplication_config(self):
-        return '<meta name="msapplication-config" content="{}"/ >'.format(
-            site_url_from_request(self.context['request']) + reverse('pages:browserconfig.xml')
-        )
+        return self.absolute_url(reverse('pages:browserconfig.xml'))
 
     def theme_color(self):
-        return '<meta name="theme-color" content="{}" />'.format(settings.BRAND_COLOR)
+        return settings.BRAND_COLOR
 
     def apple_touch_icon(self):
-        return '<link rel="apple-touch-icon" sizes="180x180" href="{}" />'.format(
-            site_url_from_request(self.context['request']) + static('favicons/apple-touch-icon.png')
-        )
+        return self.absolute_url(static('favicons/apple-touch-icon.png'))
 
     def icon(self):
-        icons = [
-            ('32x32', site_url_from_request(self.context['request']) + static('favicons/favicon-32x32.png')),
-            ('16x16', site_url_from_request(self.context['request']) + static('favicons/favicon-16x16.png')),
+        return [
+            ('32x32', self.absolute_url(static('favicons/favicon-32x32.png'))),
+            ('16x16', self.absolute_url(static('favicons/favicon-16x16.png'))),
         ]
-        return '\n'.join([
-            '<link rel="icon" type="image/png" sizes="{}" href="{}" />'.format(size, location)
-            for size, location in icons
-        ])
 
     def manifest(self):
-        return '<link rel="manifest" href="{}" />'.format(
-            site_url_from_request(self.context['request']) + reverse('pages:manifest.json')
-        )
+        return self.absolute_url(reverse('pages:manifest.json'))
 
     def mask_icon(self):
-        return '<link rel="mask-icon" href="{}" color="{}" />'.format(
-            site_url_from_request(self.context['request']) + static('favicons/safari-pinned-tab.svg'),
-            settings.BRAND_COLOR
-        )
+        return {
+            "url": self.absolute_url(static('favicons/safari-pinned-tab.svg')),
+            "color": settings.BRAND_COLOR
+        }
 
     def shortcut_icon(self):
-        return '<link rel="shortcut icon" href="{}" />'.format(
-            site_url_from_request(self.context['request']) + static('favicons/favicon.ico')
-        )
+        return self.absolute_url(static('favicons/favicon.ico'))
 
     def canonical(self):
-        return '<link rel="canonical" href="{}" />'.format(
-            site_url_from_request(self.context['request']) + self.context['request'].path
-        )
+        return self.absolute_url(self.context['request'].path)
 
 
 class RulesIndexMetaData(DefaultMetaData):
-    pass
+    def title(self):
+        return self.separator.join(['Rules', ] + self.default_title)
+
+    def keywords(self):
+        return ','.join(['rule index', ] + self.default_keywords)
+
+    def og_title(self):
+        return self.separator.join(self.default_title + ['Rules index', ])
+
+    def og_url(self):
+        return self.absolute_url(self.context['request'].path)
+
+    def twitter_title(self):
+        return self.separator.join(self.default_title + ['Rules index', ])
+
+    def twitter_description(self):
+        return self.default_description
+
+
+class RulesPageMetaData(DefaultMetaData):
+    def __init__(self, context):
+        super().__init__(context)
+        self.rule_description = self.default_description
+
+        if self.context['rule'].type != RULE_TYPES.CARD:
+            self.rule_description = self.context['rule'].clauses.filter(
+                group=CLAUSE_GROUPS.MAIN
+            ).first().current_content.content
+        else:
+            self.rule_description = "{} ({}) card clarification/errata".format(
+                self.context['rule'].name,
+                dict(CARD_TYPES.as_choices)[self.context['rule'].card_type]
+            )
+
+    def title(self):
+        return self.separator.join([self.context['rule'].name, ] + self.default_title)
+
+    def description(self):
+        return self.rule_description
+
+    def keywords(self):
+        return ','.join(
+            [self.context['rule'].name, dict(RULE_TYPES.as_choices)[self.context['rule'].type]] + self.default_keywords
+        )
+
+    def og_title(self):
+        return self.separator.join(self.default_title + [self.context['rule'].name, ])
+
+    def og_description(self):
+        return self.rule_description
+
+    def twitter_title(self):
+        return self.separator.join(self.default_title + [self.context['rule'].name, ])
+
+    def twitter_description(self):
+        return self.rule_description
 
 
 METADATA_MAPPING = {
-    "pages:index": DefaultMetaData,
+    "pages:index": RulesIndexMetaData,
     #"pages:styleguide": None,
     #"pages:resources": None,
     #"pages:contact": None,
     #"pages:wall-of-fame": None,
     #"pages:about": None,
-    #"pages:django.contrib.sitemaps.views.sitemap": None,
-    #"pages:robots_rule_list": None,
     #"books:book": None,
-    #"rules:index": None,
-    #"rules:rule": None,
+    "rules:index": RulesIndexMetaData,
+    "rules:rule": RulesPageMetaData,
     #"faqs:index": None,
 }
--- xwing_rulebook/pages/templatetags/metadata.py
+++ xwing_rulebook/pages/templatetags/metadata.py
@@ -1,13 +1,12 @@
 from django.template import Library
-from django.utils.safestring import mark_safe
 
 from pages.metadata import METADATA_MAPPING, DefaultMetaData
 
 register = Library()
 
 
-@register.simple_tag(takes_context=True)
+@register.inclusion_tag('includes/_meta.html', takes_context=True)
 def metadata(context):
     view_name = context['request'].resolver_match.view_name
     metadata_class = METADATA_MAPPING.get(view_name, DefaultMetaData)
-    return mark_safe(str(metadata_class(context)))
+    return {'metadata': metadata_class(context)}
